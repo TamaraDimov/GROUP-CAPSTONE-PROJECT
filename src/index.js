f8639321cff625a0ef4ba48be0fda15ff413d9b0
TamaraDimov <tamemojsoska@gmail.com>
2023-02-13 16:41:03 +0100

    create the hpme page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import './style.css';
 
-const url = 'https://api.tvmaze.com/shows';
+class Movies {
+  constructor() {
+    this.url = 'https://api.tvmaze.com/shows';
+    this.movies = JSON.parse(localStorage.getItem('movies')) || [];
+  }
 
-const fetchApi = async () => {
-  const req = new Request(url);
-  const res = await fetch(req);
-  const result = await res.json();
-  const scrs = result.slice(1, 7);
-  return scrs;
-};
+  fetchApi = async () => {
+    const req = new Request(this.url);
+    const res = await fetch(req);
+    const result = await res.json();
+    this.movies = result
+      .slice(0, 6)
+      .map(({ id, name, image }) => ({ id, name, image }));
+    return this.movies;
+  };
 
-fetchApi();
+  displayMovies = async () => {
+    const moviesList = document.querySelector('.movies');
+    moviesList.innerHTML = '';
+    this.movies.forEach((item) => {
+      const li = document.createElement('li');
+      li.innerHTML = ` <img src="${item.image.medium}" alt="" srcset="">
+     <div>
+     <h3>${item.name}</h3>
+     <div class="flex">
+     <span> Likes <i class="fa-solid fa-heart"></i></span>
+    <span> Comments  <i class="fa-solid fa-comment"></i></span>
+   </div>
+   </div>`;
+      moviesList.appendChild(li);
+    });
+  };
+}
+
+const movies = new Movies();
+
+movies.fetchApi().then(() => {
+  movies.displayMovies();
+});
