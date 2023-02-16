b1bdb06c45cb80086620ab9f5c8d2757a5db21ac
TamaraDimov <tamemojsoska@gmail.com>
2023-02-16 12:46:05 +0100

    ds

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
-  ],
+module.exports = (api) => {
+  const isTest = api.env('test');
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        modules: isTest ? 'commonjs' : false, // transform ES modules to commonjs for Jest
+        useBuiltIns: 'usage', // enables polyfills based on usage
+        corejs: 3, // version of core-js to use
+      },
+    ],
+    '@babel/preset-react',
+  ];
+
+  const plugins = [
+    '@babel/plugin-proposal-class-properties', // for class properties
+    '@babel/plugin-syntax-dynamic-import', // for dynamic import()
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
 };
