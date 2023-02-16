2fcaf1e2f2b8a101648d163484d64c442f7743cb
Naji-97 <elbairabennaji@gmail.com>
2023-02-16 11:54:25 +0100

    edit some linter errors

--- src/modules/counterMovie.js
+++ src/modules/counterMovie.js
@@ -1,8 +1,7 @@
-const movieCounter = ()=>{
- const movieCounter = document.querySelector('.main-section')
- const count = movieCounter.childElementCount
- console.log(count);
- return count 
-}
+const movieCounter = () => {
+  const movieCounter = document.querySelector('.main-section');
+  const count = movieCounter.childElementCount;
+  return count;
+};
 
-export default movieCounter
+export default movieCounter;
